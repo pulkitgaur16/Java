24635b0b742bf32535eaeb641e8f87df1fa68c42
Pulkit Gaur <197920899+pulkit0203@users.noreply.github.com>
2026-06-18 21:56:23 +0530

    HashMaps in progress

--- pepcoding_Java/Hashmaps.java
+++ pepcoding_Java/Hashmaps.java
@@ -32,6 +32,26 @@ public class Hashmaps {
         return ans;
     }
 
+    public static void getCommonElement(int[] arr1, int[] arr2){
+        HashMap<Integer, Integer> hm1= new HashMap<>();
+        HashMap<Integer, Integer> hm2= new HashMap<>();
+
+        for(int num: arr1){
+            if(hm1.get(num)==null){
+                hm1.put(num, 1);
+            }
+            else{
+                hm1.put(num, hm1.get(num) + 1);
+            }
+        }
+        for(int val: arr2){
+            if(hm1.containsKey(val)){
+                System.out.print(val + " ");
+                hm1.remove(val);
+            }
+        }
+    }
+
     public static void main(String[] args){
         HashMap<String, Integer> hm= new HashMap<>();
         hm.put("India", 135);
@@ -61,7 +81,24 @@ public class Hashmaps {
         //     System.out.println(key + " " + val);
         // }
 
-        char CharwithHighestfreq= highestfreq("abbbc");
-        System.out.println(CharwithHighestfreq);
+        // char CharwithHighestfreq= highestfreq("abbbc");
+        // System.out.println(CharwithHighestfreq);
+
+        Scanner scn = new Scanner(System.in);
+        int n= scn.nextInt();
+        int m= scn.nextInt();
+
+        int[] arr1= new int[n];
+        int[] arr2= new int[m];
+
+        for(int i=0; i<n; i++){
+            arr1[i]= scn.nextInt();
+        }
+
+        for(int i=0; i<m; i++){
+            arr2[i]= scn.nextInt();
+        }
+
+        getCommonElement(arr1, arr2);
     }
 }
