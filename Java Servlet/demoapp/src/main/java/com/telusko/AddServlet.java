36a701742b150f6f5be5af9c41b6ded0f0d674ac
Pulkit Gaur <197920899+pulkit0203@users.noreply.github.com>
2026-07-03 21:12:49 +0530

    initial commit

---
+++ Java Servlet/demoapp/src/main/java/com/telusko/AddServlet.java
@@ -0,0 +1,44 @@
+package com.telusko;
+
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AddServlet extends HttpServlet {
+    // post: when you want to save the data on the server
+    // public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException{
+    //     int i = Integer.parseInt(req.getParameter("num1"));
+    //     int j = Integer.parseInt(req.getParameter("num2"));
+
+    //     int k= i+j;
+
+    //     PrintWriter out = res.getWriter();
+    //     out.println("result is: "+ k);
+    // }
+
+    // get: when you want to retrieve data from the server
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
+        int i = Integer.parseInt(req.getParameter("num1"));
+        int j = Integer.parseInt(req.getParameter("num2"));
+
+        int k= i+j;
+
+        // PrintWriter out = res.getWriter();
+        // out.println("result is: "+ k);
+
+        // to call another Servlet: i) Request Dispatcher
+        // ii) Redirect
+
+        // Session Management
+
+        req.setAttribute("k", k);
+
+        RequestDispatcher rd = req.getRequestDispatcher("sq");
+        rd.forward(req, res);
+    }
+}
---
+++ Java Servlet/demoapp/src/main/java/com/telusko/SqServlet.java
@@ -0,0 +1,20 @@
+package com.telusko;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet {
+    
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+        
+        int k= (int) req.getAttribute("k");
+
+        k= k*k;
+        PrintWriter out = res.getWriter();
+        out.println("result is: " + k);
+    }
+}
