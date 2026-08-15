ae67f0bab0945c7c6a6ec5ff4330a3ca73a35b90
Pulkit Gaur <197920899+pulkitgaur16@users.noreply.github.com>
2026-08-16 01:52:19 +0530

    Solved TargetSum

--- pepcoding_Java/DynamicProgramming.java
+++ pepcoding_Java/DynamicProgramming.java
@@ -183,4 +183,41 @@ public class DynamicProgramming {
 
         return max;
     }
+
+    public static boolean TargetSum(int[] nums, int target){
+        int n = nums.length;
+        boolean[][] dp = new boolean[n+1][target+1];
+
+        for(int i=0; i<dp.length; i++){
+            for (int j = 0; j < dp[0].length; j++) {
+                if(i==0 && j==0){
+                    dp[i][j] = true;
+                }
+                else if(i==0){
+                    dp[i][j] = false;
+                }
+                else if(j==0){
+                    dp[i][j] = true;
+                }
+                else{
+                    if(dp[i-1][j] == true){
+                        dp[i][j] = true;
+                    }else{
+                        int val = nums[i-1];
+                        if(j>=val){
+                            if(dp[i-1][j-val] == true){
+                                dp[i][j] = true;
+                            }
+                        }
+                    }
+                }
+
+                if(j == target && dp[i][j] == true){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
