578a447b743f3c235d8e9be9ce074525d7cbcd8f
Pulkit Gaur <197920899+pulkitgaur16@users.noreply.github.com>
2026-08-23 19:15:48 +0530

    Max sum of subsequence without two adjacent elements

--- pepcoding_Java/DynamicProgramming.java
+++ pepcoding_Java/DynamicProgramming.java
@@ -220,4 +220,21 @@ public class DynamicProgramming {
 
         return false;
     }
+
+    public static int maxSum(int[] nums){
+        int inc = nums[0];
+        int exc = 0;
+
+        for(int i=1; i<nums.length; i++){
+            int ninc = exc + nums[i];
+            int nexc = Math.max(inc, exc);
+
+            inc = ninc;
+            exc = nexc;
+        }
+
+        int max = Math.max(inc, exc);
+
+        return max;
+    }
 }
