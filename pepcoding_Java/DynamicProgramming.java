dfae2d7cad633e4f6a0f0ac1d7b3f14a349b8ced
Pulkit Gaur <197920899+pulkitgaur16@users.noreply.github.com>
2026-09-08 02:17:14 +0530

    partitions in subset

--- pepcoding_Java/DynamicProgramming.java
+++ pepcoding_Java/DynamicProgramming.java
@@ -284,4 +284,28 @@ public class DynamicProgramming {
 
         return least;
     }
+
+    public static int partitions(int n, int k){
+        if(n==0 || k==0 || n<k){
+            return 0;
+        }
+
+        int[][] dp = new int[k+1][n+1];
+
+        for(int i=1; i<=k; i++){
+            for(int j=1; j<=n; j++){
+                if(i>j){
+                    dp[i][j] = 0;
+                }
+                else if(i==1 || (i==j)){
+                    dp[i][j] = 1;
+                }
+                else{
+                    dp[i][j] = (i*dp[i][j-1]) + dp[i-1][j-1];
+                }
+            }
+        }
+
+        return dp[k][n];
+    }
 }
