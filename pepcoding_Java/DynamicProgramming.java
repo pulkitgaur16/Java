349d4f516a1d2d692af4b7a19b8044e73410722c
Pulkit Gaur <197920899+pulkitgaur16@users.noreply.github.com>
2026-09-12 21:35:14 +0530

    Highway BillBoard problem

--- pepcoding_Java/DynamicProgramming.java
+++ pepcoding_Java/DynamicProgramming.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class DynamicProgramming {
@@ -336,5 +337,62 @@ public class DynamicProgramming {
         }
 
         return ossp;
+    }
+
+    public static int HighwayBillboard(int m, int n, int[] pos, int[] rev, int t){ // space - O(n), time- O(n^2)
+        int[] dp = new int[n];
+        dp[0] = rev[0];
+
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = rev[i];
+            int maxPrevRev = 0;
+            
+            for(int j=i-1; j>=0; j--){
+                if(pos[j] + t < pos[i]){
+                    
+                    if(dp[j] > maxPrevRev){
+                        maxPrevRev = dp[j];
+                    }
+                }
+            }
+
+            dp[i] += maxPrevRev;
+        }
+
+        int maxr = 0;
+        for(int i=0; i<n; i++){
+            if(dp[i] > maxr){
+                maxr = dp[i];
+            }
+        }
+
+        return maxr;
+    } 
+
+    public static int HighwaybillBoard(int m, int[] x, int[] rev, int t){ // space - O(m) , time - O(m)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<x.length; i++){
+            map.put(x[i], rev[i]);
+        }
+
+        int[] dp = new int[m+1];
+        dp[0] = 0;
+
+        for(int i=1; i<=m; i++){
+            if(map.containsKey(i) == false){
+                dp[i] = dp[i-1];
+            }
+            else{
+                int boardNotInstalled = dp[i-1];
+                int boardInstalled = rev[i];
+                if(i >= t+1){
+                    boardInstalled += dp[i-t-1];
+                }
+
+                dp[i] = Math.max(boardInstalled, boardNotInstalled);
+            }
+        }
+
+        return dp[m];
     }
 }
